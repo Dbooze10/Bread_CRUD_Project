ca45fb0b4581d00d93d54c1d38f8beba9eae3ff4
Dbooze10 <Desmondbooze1@gmail.com>
2023-02-27 21:01:03 -0500

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,7 +4,11 @@ const Bread = require('../models/bread.js')
 
 breads.get('/', (req, res) => {
     console.log(Bread)
-    res.send('This is the index at /breads')
+    //res.send('This is the index at /breads')
+    res.render('index', {
+        breads: Bread,
+        title: "Breads Index Page"
+    })
 })
 
 breads.get('/:arrayIndex', (req, res) => {
--- server.js
+++ server.js
@@ -4,12 +4,20 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+//Routes
 app.get('/', (req, res) => {
     res.send("<h1>An Awesome App About Breads!</h1>")
 })
 
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
+
 
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
