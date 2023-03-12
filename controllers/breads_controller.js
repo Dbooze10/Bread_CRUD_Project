4c3e233421d2f67549bdf9f88a8ff6e0c703fcb4
Dbooze10 <Desmondbooze1@gmail.com>
2023-03-12 12:01:14 -0400

    create and new routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -11,6 +11,11 @@ breads.get('/', (req, res) => {
     })
 })
 
+// NEW
+breads.get('/new', (req, res) => {
+  res.render('new')
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -18,9 +23,24 @@ breads.get('/:arrayIndex', (req, res) => {
         bread:Bread[req.params.arrayIndex]
       })
     } else {
-      res.send('NotFound')
+      res.send('404')
     }
   })
-  
+
+// CREATE
+breads.post('/', (req, res) => {
+  if(!req.body.image) {
+    req.body.image = "https://www.allrecipes.com/thmb/fm6dB2rglrJBVsltfcKtXCZFPRQ=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/27023-best-basic-sweet-bread-mfs-4-3f0ff692a57145fa847dc8d2250f4d72.jpg"
+  }
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = 'true'
+  } else {
+    req.body.hasGluten = 'false'
+  }
+  Bread.push(req.body)
+  res.redirect('/breads')
+})
+
+
 
 module.exports = breads
--- server.js
+++ server.js
@@ -9,21 +9,22 @@ app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.urlencoded({extended: true}))
 
 //Routes
 app.get('/', (req, res) => {
     res.send("<h1>An Awesome App About Breads!</h1>")
 })
 
+// Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 app.get('*', (req, res) => {
-    res.send('404')
+    res.send('<h1>404 NOT FOUND</h1>')
 })
 
-
-
+// LISTEN
 app.listen(PORT, () => {
     console.log("Listening on port:", PORT)
 })
